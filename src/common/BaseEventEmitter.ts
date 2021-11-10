b84b4eda0c4d7788c0a24ff78ba8c8f9f0030094
kkEngine <78795688+kkEngine@users.noreply.github.com>
2021-11-10 21:13:40 +0300

    Removed unused code

--- src/common/BaseEventEmitter.ts
+++ src/common/BaseEventEmitter.ts
@@ -34,36 +34,3 @@ export class BaseEventEmitter<TEvent, TValue> implements EventEmitter<TEvent, TV
     }
   }
 }
-
-// export class BaseEventEmitter<TEvent, TValue> implements EventEmitter<TEvent, TValue> {
-//   private _eventHandlers = new Map<TEvent, Set<EventEmitterHandler<TValue>>>();
-
-//   public addListener(eventName: TEvent, handler: EventEmitterHandler<TValue>): void {
-//     if (!this._eventHandlers.has(eventName)) {
-//       const handlers = new Set<EventEmitterHandler<TValue>>().add(handler);
-//       this._eventHandlers.set(eventName, handlers);
-//     } else {
-//       this._eventHandlers.get(eventName)?.add(handler);
-//     }
-//   }
-
-//   public removeListener(eventName: TEvent, handler: EventEmitterHandler<TValue>): void {
-//     if (!this._eventHandlers.has(eventName)) return;
-//     this._eventHandlers.get(eventName)?.delete(handler);
-//   }
-
-//   public removeListeners(eventName: TEvent): void {
-//     this._eventHandlers.delete(eventName);
-//   }
-
-//   public clearAll(): void {
-//     this._eventHandlers.clear();
-//   }
-
-//   public emit(eventName: TEvent, value: TValue): void {
-//     const handlers = this._eventHandlers.get(eventName);
-//     if (handlers) {
-//       handlers.forEach(handler => handler(value));
-//     }
-//   }
-// }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { BaseEventEmitter } from './common/BaseEventEmitter';
 import * as sp from "skyrimPlatform";
 import * as tk from "tick-knock";
 import { WorldCleanerSystem } from './systems/WorldCleanerSystem';
@@ -6,16 +5,10 @@ import { TimeSystem } from './systems/TimeSystem';
 import { SettingsService } from "./services/SettingsService";
 import { BaseSkympClientService } from './services/BaseSkympClientService';
 import { SkympClientService } from "./services/types";
-import { NetMessageToIface, NetMessageType, NumberMessageType, StringMessageType } from "./models/networkMessages";
-import { EventEmitter } from "./common/types";
 
 let engine = new tk.Engine();
 let settingsService = new SettingsService();
 let skympClientService: SkympClientService = new BaseSkympClientService(settingsService);
-skympClientService
-  .onMessageReceived
-  .addListener(StringMessageType.CreateActor, msg => msg.idx);
-
 
 /* Systems */
 engine.addSystem(new TimeSystem(), 1);
@@ -27,54 +20,3 @@ sp.on("update", () => {
   engine.update(Date.now() - engine_dt);
   engine_dt = Date.now();
 });
-
-
-// Let's say for example we develop an event emitter or networking system with different kinds of message types.
-// There are "types":
-type BaseType = "type1" | "type2";
-
-// And there are corresponding object types:
-interface Type1 { type1: number };
-interface Type2 { type2: number };
-
-// A generic type to automatically decide what interface we need based on BaseType passed
-type WhatIfType<T extends BaseType> =
-  T extends "type1" ? Type1 :
-  T extends "type2" ? Type2 :
-  never;
-
-
-
-interface Hkt<I, O> {
-  input: I,
-  output: O,
-}
-
-type CallHkt<F extends Hkt<any, any>, I extends F["input"]> =
-  (F & { input: I })["output"]
-
-interface WhatIfTypeHkt extends Hkt<BaseType, Type1 | Type2> {
-  output: WhatIfType<this["input"]>
-}
-
-// Just a generic type with two parameters. Required for our example function myFunc
-// Note that here T1 is one of "types" (BaseType)
-// T2 is one of interfaces (Type1, Type2)
-interface superObject<F extends Hkt<any, any>> {
-  add<T extends F["input"]>(value1: T, callback: (value2: CallHkt<F, T>) => void): this;
-}
-
-let myFunc = (): superObject<WhatIfTypeHkt> => {
-  throw new Error();
-};
-
-myFunc().add("type1", (val) => val.type1);
-myFunc().add("type2", (val) => val.type2);
-
-
-class SuperObj<TEvent, TValue> implements superObject<Hkt<TEvent, TValue>> {
-  add<T extends TEvent>(value1: T, callback: (value2: TValue) => void): this {
-    throw new Error('Method not implemented.');
-  }
-
-}
